63127d847bfda4be9f2a3c33a9e212a636a101c5
mauthier-bit <mauthier@web.de>
2026-09-09 21:12:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker – macht die Tafel offline-fähig (App-Shell + Bibliotheken cachen). */
-const CACHE = 'tafel-v113';
+const CACHE = 'tafel-v114';
 const ASSETS = [
   './',
   'index.html',
